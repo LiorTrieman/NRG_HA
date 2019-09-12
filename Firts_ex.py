20920c79cef360b45217ee77a34158cad62c5473
LiorTrieman <45787086+LiorTrieman@users.noreply.github.com>
2019-09-12 09:46:22 +0300

    Upload submitted version
    * submitted as a home assignment

--- Firts_ex.py
+++ Firts_ex.py
@@ -1,33 +1,78 @@
 # PART 1 - ARRAY DESIGN #
 # --------------------- #
+"""
+# Open and read a fasta file
+# analyse with regular expressions
+# output the following:
 
+a. Marker name - e.g. USDA_SNP0012
+b. Marker ID - e.g. i00001Gh
+c. Marker allele 1 - e.g. C
+d. Marker allele 2 - e.g. T
+e. Full marker sequence with allele 1 - e.g. TGCAGAACACAGA...C...AAGTAAAA
+f. Full marker sequence with allele 2 - e.g. TGCAGAACACAGA...T...AAGTAAAA
+g. Marker length - e.g. 63
+h. Position of SNP in the marker (0-based index) - e.g. 54
+"""
+import re  # in order to use regular expressions
+import numpy as np  # arrange data
+import matplotlib.pyplot as plt  # create plots and graphs
+from collections import Counter
+
+"""
+with open('TAMU_SNP63K_69997.fasta', 'r') as file:  # read the fasta file
+    data = file.read().replace('\n', '')
+# print(type(data))  # view data type
+"""
+
+
+def read_fasta(fasta_file):
+    with open(fasta_file, 'r') as file:  # read the fasta file
+        data = file.read().replace('\n', '')
+    return data
+    # print(type(data))  # view data type
+
+
+def put_allele(text, allele):  # replace one of [Y, M, K, R, W, S] with the alleles
+    for ch in ['Y', 'M', 'K', 'R', 'W', 'S']:
+        if ch in text:
+            text = text.replace(ch, allele)  # replace with one of the alleles
+    return text
+
+
+def find_snp_ind(text):  # return the index of the snp in the marker
+    for ch in ['Y', 'M', 'K', 'R', 'W', 'S']:
+        snp_ind = text.find(ch)
+        if snp_ind != -1:
+            break  # check if one of the 'ch' above is inside the string
+    return snp_ind
+
+
+def cut_head_of_snp_string(raw_SNP):  # get only the snp from the raw string
+    snp = raw_SNP[2:]
+    return snp
+
+
+def auto_label(rects):  # Attach a text label above each bar
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+
+def cut_head_of_marker_seq_string(raw_str):  # get list of lengths from raw string
+    length = raw_str[2:]
+    return length
 
 #   QUESTIONS 1 - CREATE A CSV FILE FROM EXTRACTED DATA  #
 # ------------------------------------------------------ #
-def main():
-    """
-    # Open and read a fasta file
-    # analyse with regular expressions
-    # output the following:
 
-    a. Marker name - e.g. USDA_SNP0012
-    b. Marker ID - e.g. i00001Gh
-    c. Marker allele 1 - e.g. C
-    d. Marker allele 2 - e.g. T
-    e. Full marker sequence with allele 1 - e.g. TGCAGAACACAGA...C...AAGTAAAA
-    f. Full marker sequence with allele 2 - e.g. TGCAGAACACAGA...T...AAGTAAAA
-    g. Marker length - e.g. 63
-    h. Position of SNP in the marker (0-based index) - e.g. 54
-    """
-    import re  # in order to use regular expressions
-    import numpy as np  # arrange data
-    import matplotlib.pyplot as plt  # create plots and graphs
-    from collections import Counter
 
-    with open('TAMU_SNP63K_69997.fasta', 'r') as file:  # read the fasta file
-        data = file.read().replace('\n', '')
-    # print(type(data))  # view data type
-
+if __name__ == "__main__":
+    data = read_fasta('TAMU_SNP63K_69997.fasta')
     marker_name = re.findall(r'name=\S{1,50}', data)  # a.Marker name
     # print("len marker name: ", len(marker_name))  # to verify that is finds all names
     marker_ID = re.findall(r'63K_i\d{5}\w{2}', data)  # b.Marker ID
@@ -37,16 +82,11 @@ def main():
     # print(len(marker_alleles_all))
     # need to separate each allele from the above string
     all_marker_len = len(marker_alleles_all)
-
-    print("number of different markers = ", all_marker_len)
-    print("typ: ", type(marker_alleles_all))
-
     # to find  Full marker sequence we should search for :spaceX2, {A,G,C,T, and Y,M,K,R,W,S} and stop at "enter" or "<"
 
     full_marker_seq_raw = re.findall(r'\s\s[A, G, C, T, Y, M, K, R, W, S]*', data)  # Full marker sequence with allele 1
     # print(full_marker_seq_raw[0:10])
     full_marker_seq_raw_len = len(full_marker_seq_raw)
-    # print("len: ", full_marker_seq_raw_len)
 
     # Creating a CSV file
     download_dir = "Markers_Data.csv"  # where you want the file to be downloaded to
@@ -55,26 +95,8 @@ def main():
      Full_marker_sequence_allele_2,Marker_length, Position_of_SNP\n"
     csv.write(columnTitleRow)
 
-    flag_more_than_one_snp = [0] * len(marker_alleles_all)
-
-
-    def put_allele(text, allele):  # replace one of [Y, M, K, R, W, S] with the alleles
-        for ch in ['Y', 'M', 'K', 'R', 'W', 'S']:
-            if ch in text:
-                text = text.replace(ch, allele)  # replace with one of the alleles
-        return text
-
-
-    def find_snp_ind(text):  # return the index of the snp in the marker
-        for ch in ['Y', 'M', 'K', 'R', 'W', 'S']:
-            snp_ind = text.find(ch)
-            if snp_ind != -1:
-                break  # check if one of the 'ch' above is inside the string
-        return snp_ind
     # HAVEN'T FOUND A MARKER WITH MORE THAN ONE WILDCARD CHARACTER
 
-
-    # print(all_marker_len)
     for index in range(0, all_marker_len):  # finding the items
         marker_name_current_raw = marker_name[index]
         marker_name_current = marker_name_current_raw[5:]  # cut relevant part of the string
@@ -89,11 +111,10 @@ def main():
         full_marker_seq_allele_2 = put_allele(full_marker_seq, Allele_2)  # f. Full marker sequence with allele 2
         marker_length = len(full_marker_seq_allele_2)  # g. Marker length
         ind_snp = find_snp_ind(full_marker_seq)  # h. Position of SNP in the marker (0-based index)
-
-        row = marker_name_current + "," + marker_ID_current + "," + Allele_1 + "," + Allele_2 + "," + full_marker_seq_allele_1 \
-              + "," + full_marker_seq_allele_2 + "," + str(marker_length) + "," + str( ind_snp) + "," + "\n"
+        row = marker_name_current + "," + marker_ID_current + "," + Allele_1 + "," + Allele_2 + "," +\
+            full_marker_seq_allele_1 + "," + full_marker_seq_allele_2 + "," + str(marker_length) + "," \
+            + str(ind_snp) + "," + "\n"
         csv.write(row)  # adding a raw to file each 'for' iteration
-
 
     #  QUESTIONS 2 - CREATE 2 FASTA FILE ONE FOR EACH ALLELES  #
     # -------------------------------------------------------- #
@@ -105,18 +126,16 @@ def main():
     # create a dict from these two lists
     # write then into fasta file """
     list_name = []
-    list_seq = []
 
     for index in range(0, all_marker_len):  # finding the items
         marker_name_current_raw = marker_name[index]
         marker_name_current = marker_name_current_raw[5:]
         list_name.append(marker_name_current)
 
-
-    o_file = open("fasta_allele_1.txt", "w")
+    o_file = open("fasta_allele_1.fasta", "w")
     o_file.write(">")  # write a title of names
     for index in range(len(list_name)):
-        o_file.write(list_name[index] + ",")
+        o_file.write(list_name[index] + " ")
 
     for index in range(len(list_name)):
         Allele = marker_alleles_all[index]
@@ -126,11 +145,11 @@ def main():
         full_marker_seq_allele_1 = put_allele(full_marker_seq, Allele_1)  # e. Full marker sequence with allele 1
         o_file.write(">" + full_marker_seq_allele_1 + "\n")
     o_file.close()
-    o_file = open("fasta_allele_2.txt", "w")
+    o_file = open("fasta_allele_2.fasta", "w")
     # write a title of names:
     o_file.write(">")
     for index in range(len(list_name)):
-        o_file.write(list_name[index] + ",")
+        o_file.write(list_name[index] + " ")
 
     for index in range(len(list_name)):
         Allele = marker_alleles_all[index]
@@ -141,20 +160,13 @@ def main():
         o_file.write(">" + full_marker_seq_allele_2 + "\n")
     o_file.close()
 
-
-    #  QUESTIONS 3 - PLOTTING THE DATA #
+    # QUESTIONS 3 - PLOTTING THE DATA #
     # -------------------------------- #
 
     # Bar Chart for Frequencies of SNP types (C/T, C/G. C/A, A/T, A/G, G/T)
     # marker_alleles_all -raw list of all SNP types, need to extract freqs
     num_total_snp = len(marker_alleles_all)
     list_of_snp = []
-
-
-    def cut_head_of_snp_string(raw_SNP):  # get only the snp from the raw string
-        SNP = raw_SNP[2:]
-        return SNP
-
 
     for index in range(0, num_total_snp):  # get the list of all snp
         snp_type = cut_head_of_snp_string(marker_alleles_all[index])
@@ -163,24 +175,33 @@ def main():
     snp_freqs = []
     snp_objects = Counter(list_of_snp).keys()  # equals to list(set(words))
     snp_counts = Counter(list_of_snp).values()  # counts the elements' appearances
+    print(snp_objects)
+    print(snp_counts)
     for index in snp_counts:
         freq = index/num_total_snp
         snp_freqs.append(freq)
     y_pos = np.arange(len(snp_objects))
     plt.figure(1)  # new figure
-    fig = plt.bar(y_pos, snp_freqs, align='center', alpha=0.5)
+    plt.bar(y_pos, snp_freqs, align='center', alpha=0.5)
     plt.xticks(y_pos, snp_objects)
     plt.ylabel('Frequencies')
     plt.title('Frequencies of SNP types')
+    x = np.arange(len(snp_objects))  # the label locations
+    width = 0.35  # the width of the bars
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x, snp_counts, width)
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Count of SNP')
+    ax.set_title('Total count of SNP types')
+    ax.set_xticks(x)
+    ax.set_xticklabels(snp_objects)
+    auto_label(rects1)
+    fig.tight_layout()
     plt.show()
+
     # Marker length - Histogram
     list_of_marker_length = []
-
-
-    def cut_head_of_marker_seq_string(raw_str):  # get list of lengths from raw string
-        length = raw_str[2:]
-        return length
-
 
     for index in range(0, num_total_snp):
         marker_str = cut_head_of_marker_seq_string(full_marker_seq_raw[index])
@@ -200,17 +221,10 @@ def main():
             max_len = len(full_marker_seq)  # update the maximum
         if len(full_marker_seq) > 0:
             norm_snp_list.append(ind_snp/int(len(full_marker_seq)))
-        # print(index)
-        # print(len(full_marker_seq))
         if len(full_marker_seq) == 0:
             count_zero += 1  # FOUND 8 GENE WITH SEQ LEN == ZERO - SHOULD BE CHECKED
-    print(count_zero)
-
-
-    # print(list_of_marker_length)  # test print
-    # print(len(list_of_marker_length))  # test print
     plt.figure(2)
-    fig_lengths = plt.hist(list_of_marker_length, range=(0, max_len), density=False, histtype='bar', align='mid', orientation='vertical')
+    plt.hist(list_of_marker_length, range=(0, max_len), density=False, histtype='bar', align='mid', orientation='vertical')
     plt.ylabel('Marker Lengths counts')
     plt.xlabel('Marker Lengths (number of Nucleotide)')
     plt.title('Marker Lengths Histogram')
@@ -226,4 +240,4 @@ def main():
     plt.show()
 
 
-main()
+
